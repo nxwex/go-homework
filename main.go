6f1dcdee68dc20ad9f12fa5bdfa12abc39ee7fe1
wex <notwexx@gmail.com>
2026-02-09 15:18:24 +0300

    Добавил рассчет ИМТ и собрал в .exe файл

--- main.go
+++ main.go
@@ -2,9 +2,18 @@ package main
 
 import (
 	"fmt"
+	"math"
 )
 
 func main() {
-	city := "Moscow"
-	fmt.Println("Variable: city has value: " + city)
+	const IMTPower = 2
+	var userHeight float64
+	var userKg float64
+	fmt.Println("=== Калькулятор индекса массы тела ===")
+	fmt.Print("Введите свой рост в метрах (формат x.xx): ")
+	fmt.Scan(&userHeight)
+	fmt.Print("Введите свой вес в кг (формат x.xx): ")
+	fmt.Scan(&userKg)
+	IMT := userKg / math.Pow(userHeight, IMTPower)
+	fmt.Printf("Рост: %2.f, вес: %2.f, ваш индекс массы тела: %2.f\n", userHeight, userKg, IMT)
 }
