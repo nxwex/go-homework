b68a7fd9ac81e116e27d8572ef300764bf4b9d03
wex <notwexx@gmail.com>
2026-02-09 18:19:54 +0300

    Декомпозировал код, добавил две новые функции

--- 1-converter/main.go
+++ 1-converter/main.go
@@ -5,7 +5,24 @@ import "fmt"
 func main() {
 	const ConvUSDtoEUR float64 = 0.84
 	const ConvUSDtoRUB float64 = 77.50
-	var eur float64 = 10.0
-	rub := eur / ConvUSDtoEUR * ConvUSDtoRUB
-	fmt.Printf("Курс доллара к евро = %v, курс доллара к рублю = %v, количество евро = %2.f, получается %2.f руб.\n", ConvUSDtoEUR, ConvUSDtoRUB, eur, rub)
+	const ConvEURtoRUB float64 = ConvUSDtoRUB / ConvUSDtoEUR
+	var volume float64
+	var pairFirst string
+	var pairSecond string
+	calculatePair(inputUser(volume, pairFirst, pairSecond))
+}
+
+func inputUser(volume float64, pairFirst, pairSecond string) (float64, string, string) {
+	fmt.Print("Введите количество средств для конвертации: ")
+	fmt.Scan(&volume)
+	fmt.Print("Из какой валюты (RUB, USD, EUR): ")
+	fmt.Scan(&pairFirst)
+	fmt.Print("В какую валюту? (RUB, USD, EUR): ")
+	fmt.Scan(&pairSecond)
+	return volume, pairFirst, pairSecond
+}
+
+func calculatePair(volume float64, pairFirst, pairSecond string) {
+	fmt.Printf("Сумма: %.2f, из валюты %s в валюту %s = в разработке..\n", volume, pairFirst, pairSecond)
+	// Как и просили в курсе, пока без действий просто вывод, поскольку разберем это дальше
 }
